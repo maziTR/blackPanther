12e953e6ab6114c04708506c37d226b3571e36f7
Anna W <aweinmacher@gmail.com>
2018-02-07 19:00:11 +0200

    main artist render correct

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SearchForm from './SearchForm';
 import ArtistBox from './ArtistBox';
@@ -8,17 +7,24 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.onSubmitSearchForm = this.onSubmitSearchForm.bind(this);
-    this.state = {artistName: ""};
+    this.state = { artistName: "" };
   }
   onSubmitSearchForm(data) {
     this.setState({ artistName: data });
   }
   render() {
-    return (
+    if (this.state.artistName) {
+      return (
+        <div className="App">
+        <h1>Black Panther</h1>
+        <SearchForm onSubmitSearchForm={this.onSubmitSearchForm} />
+        <ArtistBox artistName={this.state.artistName}/>
+      </div>
+      )
+    } else return (
       <div className="App">
-        <SearchForm onSubmitSearchForm={this.onSubmitSearchForm}/>
-        {/* will be a browser router */}
-        <ArtistBox artistName={this.state.artistName}/> 
+        <h1>Black Panther</h1>
+        <SearchForm onSubmitSearchForm={this.onSubmitSearchForm} />
       </div>
     );
   }
