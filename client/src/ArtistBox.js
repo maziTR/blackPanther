fcf52c9bfe3066ce8496f19ecf4d3f3dfa2cbe67
mazi tar <mazi.tr@gmail.com>
2018-02-08 17:08:22 +0200

    final

--- client/src/ArtistBox.js
+++ client/src/ArtistBox.js
@@ -12,7 +12,7 @@ class ArtistBox extends Component {
                 <div className="grid-item">
                 <img className="main-artist-img" alt={this.props.artistInfo.artistName} src={this.props.artistInfo.artistImg || "https://placebear.com/300/300"}></img> 
                 <p className="main-artist-title"> {this.props.artistInfo.artistName || "Leonard Cohen"} </p>
-                <button className="play-button"><img alt="play-button" src={"http://www.pngmart.com/files/3/Play-Button-Transparent-PNG.png"}></img></button>
+                <div className="play-button"><img alt="play-button" src={"http://www.pngmart.com/files/3/Play-Button-Transparent-PNG.png"}></img></div>
                 <h3 className="main-artist-song">{this.props.artistInfo.song || "Chelsea Hotel"}</h3>
                 </div>
                 <RelArtistsList relArtists={this.props.artistInfo.relArtists} searchArtist={this.props.searchArtist}/>
